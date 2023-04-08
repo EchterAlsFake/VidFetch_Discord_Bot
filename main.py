949d9a631cea890b7b090a7148d9f92a168fd078
EchterAlsFake <EchterAlsFake@proton.me>
2023-04-08 17:49:01 +0200

    Added Playlist Support Added German Language support Added Threading for all functions Code Refactoring
    :)

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ https://github.com/EchterAlsFake/VidFetch_Discord_Bot
 Thanks :)
 
 """
-
-
 class Setup():
 
     def __init__(self):
@@ -37,15 +35,14 @@ class Setup():
     def configure(self):
         dotenv.load_dotenv()
 
-
-
 Setup()
-
 client_secret = os.getenv("TOKEN")
-intents = discord.Intents.all()
+intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
-
-
+download_queue = queue.Queue()
+num_threads = 12
+semaphore = threading.Semaphore(num_threads)
 
 
 def error(e):
@@ -55,29 +52,7 @@ def error(e):
         f.write(f"{e}\n")
         f.close()
 
-@bot.event
-async def on_ready():
-    print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + "Bot is ready!")
-
-    try:
-        await bot.tree.sync()
-        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTCYAN_EX + f"Synced Application Commands.")
-
-    except Exception as e:
-        error(e)
-
-download_queue = queue.Queue()
-num_threads = 12
-semaphore = threading.Semaphore(num_threads)
-
-
-@bot.tree.command(name="download", description="Downloads a YouTube Video and sends it back as .m4a")
-async def hello(interaction: discord.Interaction, url : str):
-    await interaction.response.send_message("Proceeding your request...")
-    user = interaction.user
-    download_queue.put(url)
-
-    def download(user):
+def download_english(user):
         while True:
             try:
                 url = download_queue.get(block=False)
@@ -113,27 +88,121 @@ async def hello(interaction: discord.Interaction, url : str):
 
                     if title == "passwd" or title == "etc":
                         print(title)
-                        print(Fore.LIGHTRED_EX + f"Someone probably tried to break into the system.  USER: {interaction.user}")
+                        print(Fore.LIGHTRED_EX + f"Someone probably tried to break into the system.  USER: {user}")
 
                     else:
                         print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + f"Downloading: {title}")
                         audio_stream.download(filename=f"{title}.m4a")
                         print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + f"Downloaded: {title}.m4a")
-                        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTMAGENTA_EX + f"Sending to: {interaction.user}...")
+                        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTMAGENTA_EX + f"Sending to: {user}...")
                         bot.loop.create_task(user.send(file=discord.File(f"{title}.m4a")))
 
                 except Exception as e:
                     error(e)
                     bot.loop.create_task(user.send(f"Something went wrong.  Here is the error: {e}"))
 
-    t = threading.Thread(target=download, args=(user,))
-    t.start()
+def download_german(user):
+        while True:
+            try:
+                url = download_queue.get(block=False)
+
+            except queue.Empty:
+                print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + f"Warteschlange ist leer.  Es wird 5 Sekunden gewartet.")
+                break
+
+            with semaphore:
+                try:
+                    y = YouTube(url)
+
+                except Exception as e:
+                    error(e)
+                    bot.loop.create_task(user.send(
+                        "Falsche URL.  Falls du dir sicher bist, dass die URL richtig ist, dann gehe bitte auf die GitHub Seite damit du schauen kannst, ob du alles korrekt getan hast."))
+                    bot.loop.create_task(user.send(f"Fehlermeldung für den Debugging Prozess: {e}"))
+
+                try:
+
+                    print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTWHITE_EX + f"Processing: {url}")
+
+                    y = YouTube(url)
+                    title = y.title
+                    audio_stream = y.streams.get_audio_only()
+                    disallowed_symbols = ["/", "\\", ":", "*", "?", "\"", "<", ">", "|"]
+
+                    for disallowed_symbol in disallowed_symbols:
+                        title = title.replace(disallowed_symbol, "")
+
+                    if title == "passwd" or title == "etc":
+                        print(title)
+                        print(
+                            Fore.LIGHTRED_EX + f"Es wurde versucht eine Sicherheitslücke auszunutzen.   Nutzer: {user}")
+
+                    else:
+                        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + f"Downloading: {title}")
+                        audio_stream.download(filename=f"{title}.m4a")
+                        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + f"Downloaded: {title}.m4a")
+                        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTMAGENTA_EX + f"Sending to: {user}...")
+                        bot.loop.create_task(user.send(file=discord.File(f"{title}.m4a")))
+
+                except Exception as e:
+                    error(e)
+                    bot.loop.create_task(user.send(f"Es gab einen unerwarteten Fehler: {e}"))
+
+@bot.event
+async def on_ready():
+    print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTYELLOW_EX + "Bot is ready!")
+
+    try:
+        await bot.tree.sync()
+        print(Fore.LIGHTGREEN_EX + "[+]" + Fore.LIGHTCYAN_EX + f"Synced Application Commands.")
+
+    except Exception as e:
+        error(e)
+
+@bot.tree.command(name="download", description="Languages:  de = German, en = English")
+async def hello(interaction: discord.Interaction, url : str, language : str):
+    await interaction.response.send_message("Proceeding your request...")
+    user = interaction.user
+    download_queue.put(url)
 
 
+
+    if language == "de":
+        t = threading.Thread(target=download_german, args=(user,))
+        t.start()
+
+    elif language == "en":
+        t = threading.Thread(target=download_english, args=(user,))
+        t.start()
+
+    else:
+        t = threading.Thread(target=download_english, args=(user,))
+        t.start()
+
+@bot.tree.command(name="playlist", description="Languages:  de = German, en = English")
+async def playlist(interaction: discord.Interaction, url : str, language : str):
+    await interaction.response.send_message("Proceeding your request...  (This can take a long time! Please be patient...)")
+    user = interaction.user
+    p = Playlist(url)
+    for video in p.video_urls:
+        download_queue.put(video)
+
+    if language == "de":
+        t = threading.Thread(target=download_german, args=(user,))
+        t.start()
+
+    elif language == "en":
+        t = threading.Thread(target=download_english, args=(user,))
+        t.start()
+
+    else:
+        t = threading.Thread(target=download_english, args=(user,))
+        t.start()
 
 @bot.tree.command(name="help", description="Shows a simple tutorial message.")
 async def help(interaction: discord.Interaction):
-    await interaction.response.send_message("""
+    def send_help_thread():
+        await interaction.response.send_message("""
 
 Commands:
 
@@ -143,9 +212,13 @@ Commands:
 
 """)
 
+    t = threading.Thread(target=send_help_thread)
+    t.start()
+
 @bot.tree.command(name="credits", description="Shows all sources.")
 async def credits(interaction: discord.Interaction):
-    await interaction.response.send_message("""
+    def send_credits_thread():
+        await interaction.response.send_message("""
 
 Vid Fetch is the Discord Bot of the Vid Fetch YouTube Downloader, which were
 both created by EchterAlsFake in 2023. The Discord Bot itself is licensed under the 
@@ -163,5 +236,9 @@ I can not guarantee for 24/7 Support or functionality!
 
 Have a great day :) 
 """)
+
+
+    t = threading.Thread(target=send_credits_thread)
+    t.start()
 
 bot.run(client_secret)
